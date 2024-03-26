7f2adc0f3834fa164b84a12f019459a34100ddba
Stephane Reinstein <steph@sntial.co.za>
2024-03-26 15:00:11 +0200

    version bump

--- glueup.php
+++ glueup.php
@@ -8,7 +8,7 @@
  * Author URI:      https://bigambitions.co.za
  * Text Domain:     glueup
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         0.1.5
  * GitHub Plugin URI: divemasterza/glueup-directory-wp
  *
  * @package         Glueup
